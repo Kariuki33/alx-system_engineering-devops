67d825f8ea56516bd1385df61b31ad3b2648c1bb
Kariuki33 <110014204+Kariuki33@users.noreply.github.com>
2023-02-02 09:30:45 +0300

    Update 1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,2 +1,16 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hbt{2,5}n/).join
+
+# Check if an argument is passed, and if the argument matches the pattern "School"
+
+if ARGV.empty?
+  puts "Usage: regex_script.rb {argument}"
+  exit 1
+end
+
+arg = ARGV[0]
+
+if arg.match?(/School/)
+  puts "Match found"
+else
+  puts "Match not found"
+end
