d6f79c1f3137ca2b3916c82ff91a7cf188b318df
Kariuki33 <danqarees@gmail.com>
2023-02-16 09:46:54 +0300

    Update 2-repetition_token_1.rb

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
-# A regular expression that is matches a given pattern
+# ./2-repetition_token_1.rb
+# Prints the string that matches the regular expression holberton
 puts ARGV[0].scan(/hb?tn/).join
