7396d199e6959eec230b041a7d72aefc9a1a283a
Kariuki33 <danqarees@gmail.com>
2023-02-16 09:44:53 +0300

    Update 1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
-# A regular expression that is matches a given pattern
+# Prints all the words that start with h, end with n, and have between 2 and 5 t's in between.
+# ./1-repetition_token_0.rb
 puts ARGV[0].scan(/hbt{2,5}n/).join
