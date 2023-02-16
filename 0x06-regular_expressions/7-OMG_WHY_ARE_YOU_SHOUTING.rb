e7464ca6775fc0054e16046795ad1c0b5182b942
Kariuki33 <danqarees@gmail.com>
2023-02-16 09:51:14 +0300

    Update 7-OMG_WHY_ARE_YOU_SHOUTING.rb

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
-# A regular expression that is matches only capital letters
+# The regular expression must be only matching: capital letters
+# It prints all the capital letters in the string.
 puts ARGV[0].scan(/[A-Z]/).join
